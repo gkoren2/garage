c10fdcf35b8edad4e80c619a38d38a82ed4e4015
gkoren2 <guy.koren@intel.com>
2021-08-10 13:51:38 +0300

    + added walker2d_rand_params.py and hopper_rand_params.py

---
+++ _labexp/my_envs/hopper_rand_params.py
@@ -0,0 +1,60 @@
+import os
+import sys
+proj_root_dir = os.path.dirname(os.path.dirname(os.path.realpath('.')))   # project root dir
+if proj_root_dir not in sys.path:
+    sys.path.insert(0,proj_root_dir)
+######################
+
+import numpy as np
+from _labexp.my_envs.base import RandomEnv
+from gym import utils
+
+class HopperRandParamsEnv(RandomEnv, utils.EzPickle):
+    def __init__(self, log_scale_limit=3.0):
+        RandomEnv.__init__(self, log_scale_limit, 'hopper.xml', 4)
+        utils.EzPickle.__init__(self)
+
+    def _step(self, a):
+        posbefore = self.data.qpos[0]
+        self.do_simulation(a, self.frame_skip)
+        posafter, height, ang = self.data.qpos[0:3]
+        alive_bonus = 1.0
+        reward = (posafter - posbefore) / self.dt
+        reward += alive_bonus
+        reward -= 1e-3 * np.square(a).sum()
+        s = self.state_vector()
+        done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
+                    (height > .7) and (abs(ang) < .2))
+        ob = self._get_obs()
+        return ob, reward, done, {}
+
+    def _get_obs(self):
+        return np.concatenate([
+            self.data.qpos.flat[1:],
+            np.clip(self.data.qvel.flat, -10, 10)
+        ])
+
+    def reset_model(self):
+        qpos = self.init_qpos + self.np_random.uniform(low=-.005, high=.005, size=self.model.nq)
+        qvel = self.init_qvel + self.np_random.uniform(low=-.005, high=.005, size=self.model.nv)
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+    def viewer_setup(self):
+        self.viewer.cam.trackbodyid = 2
+        self.viewer.cam.distance = self.model.stat.extent * 0.75
+        self.viewer.cam.lookat[2] += .8
+        self.viewer.cam.elevation = -20
+
+if __name__ == "__main__":
+
+    env = HopperRandParamsEnv()
+    tasks = env.sample_tasks(40)
+    while True:
+        env.reset()
+        env.set_task(np.random.choice(tasks))
+        print(env.model.body_mass)
+        for _ in range(100):
+            env.render()
+            env.step(env.action_space.sample())  # take a random action
+
